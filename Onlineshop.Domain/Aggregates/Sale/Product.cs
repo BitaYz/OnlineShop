de68d605da6afd0cccbe4f9a23bcb7206a55ed31
Bita_yz <yazdiameneh71@gmail.com>
2024-02-29 12:45:49 +0330

    update

--- Onlineshop.Domain/Aggregates/Sale/Product.cs
+++ Onlineshop.Domain/Aggregates/Sale/Product.cs
@@ -1,14 +1,14 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Onlineshop.Domain.Aggregates.Sale
-//{
-//    public class Product : MainEntityBase
-//    {
-//        public Guid ProductCategoryId { get; set; }
-//        public decimal UnitPrice { get; set; }
-//    }
-//}
+namespace Onlineshop.Domain.Aggregates.Sale
+{
+    public class Product : MainEntityBase
+    {
+        public Guid ProductCategoryId { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
